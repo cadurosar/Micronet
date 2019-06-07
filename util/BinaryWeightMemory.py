38d029eb987e0ab90bf511e834eba0c7b75fe74f
Sebastien Henwood <henwood@pcys31.grm.polymtl.ca>
2019-06-07 10:18:38 -0400

    bwm fix

--- util/BinaryWeightMemory.py
+++ util/BinaryWeightMemory.py
@@ -2,14 +2,15 @@ import torch, torchvision, torch.nn as nn
 import math, numpy as np
 
 class BinaryWeightMemory():
-  def __init__(self, model:nn.Module):
+  def __init__(self, model:nn.Module, BWM=True):
     """
     Hold the pointer to the weights and the quantized representation associated
     From Courbariaux & al. 2015
     """
     self.saved_params = []
     self.actual_params = []
-    self.params = 0  
+    self.params = 0
+    self.BWM = BWM
     for m in model.modules():
       if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
         self.saved_params.append(m.weight.data.clone())
@@ -17,7 +18,7 @@ class BinaryWeightMemory():
         self.params += 1
 
     
-  def binarize(self, BWN=True, determinist = True):
+  def binarize(self, determinist = True):
     """ 
     Modify the weights to show their binary counterpart at inference time
     """
@@ -29,7 +30,7 @@ class BinaryWeightMemory():
       else:
         """ From Courbariaux & al. 2015, w_b = +1 with p=hardsigmoid(w), -1 with q = 1-p """
         raise ValueError('Stochastic binarization is not supported')
-      if BWN: quantized *= torch.mean(true_value)
+      if self.BWN: quantized *= torch.mean(true_value)
       self.actual_params[i].data.copy_(quantized)
   
   def restore(self):
@@ -40,8 +41,9 @@ class BinaryWeightMemory():
     """ From Courbariaux & al. 2015, 2.4 - Clip weights after update to -1;1 
     since it doesn't impact the sign() ops while preventing overly large weights
     """
-    for i in range(self.params):
-      self.actual_params[i].data.copy_(torch.clamp(self.actual_params[i], min=-1, max=1))
+    if not self.BWM:
+        for i in range(self.params):
+            self.actual_params[i].data.copy_(torch.clamp(self.actual_params[i], min=-1, max=1))
       
   def __str__(self):
     """ Return a string representing the first param of the weight manager """
