bf3b0823ed839c65172a1f588556ea237af5313a
Ghouthi Boukli <bouklihg@power92.server.mila.quebec>
2019-08-04 13:56:31 -0400

    correct student

--- Integrated/student.py
+++ Integrated/student.py
@@ -12,30 +12,30 @@ import argparse
 from models import *
 from utils import progress_bar, load_data, train, test
 from torch.optim.lr_scheduler import MultiStepLR
-
+import mobilenet
 
 def main():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    clean_trainloader, trainloader, testloader = load_data(1000, cutout=False, batch_clean=100)
-    teacher_file = "checkpoint/resnet11010.pth"
-    teacher = torch.load(file)["net"].module
-    net = ResNet14(wide=4,first_layer=16)
+    clean_trainloader, trainloader, testloader = load_data(32, cutout=False, batch_clean=100)
+    teacher_file = "/network/tmp1/rosarkoc/resnet11010-2.pth"
+    teacher = torch.load(teacher_file)["net"].module
+    net = mobilenet.mobilenet_v2(width_mult=1.4) # ResNet14(wide=4,first_layer=16)
     net = net.to(device)
     if device == 'cuda':
         net = torch.nn.DataParallel(net)
         cudnn.benchmark = True
     optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
-    scheduler = MultiStepLR(optimizer, milestones=[100, 150], gamma=0.1)
-    for epoch in range(200):
+    scheduler = MultiStepLR(optimizer, milestones=[100, 175], gamma=0.1)
+    for epoch in range(250):
         print('Epoch: %d' % epoch)
-        train(net,trainloader,scheduler, device, optimizer,cutmix=True)
-        test(net,testloader, device, save_name="teacher_11010_student_144")
+        train(net,trainloader,scheduler, device, optimizer,cutmix=True, teacher = teacher)
+        test(net,testloader, device, save_name="teacher_11010_student_mobilenet")
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9, weight_decay=1e-4)
     scheduler = MultiStepLR(optimizer, milestones=[50, 150], gamma=0.1)
-    for epoch in range(100):
+    for epoch in range(5):
         print('Epoch: %d' % epoch)
         train(net,clean_trainloader,scheduler, device, optimizer,cutmix=False,mixup_alpha=0,teacher=teacher)
-        test(net,testloader, device, save_name="Teacher11010_student144")
+        test(net,testloader, device, save_name="teacher_11010_student_mobilenet-2")
 if __name__ == "__main__":
     main()
 
